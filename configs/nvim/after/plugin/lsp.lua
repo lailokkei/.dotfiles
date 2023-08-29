f43302a4dd1cb3975a04a7d17e4733e5e8d370c4
toodemhard <lailokkay@gmail.com>
2023-08-29 13:59:23 +0100

    cmp confirm replace

--- configs/nvim/after/plugin/lsp.lua
+++ configs/nvim/after/plugin/lsp.lua
@@ -12,17 +12,6 @@ lsp.ensure_installed({
     'lua_ls',
 })
 
-require('lspconfig').html.setup {
-    init_options = {
-        configurationSection = { "html", "css", "javascript" },
-        embeddedLanguages = {
-            css = true,
-            javascript = true
-        },
-        provideFormatter = true
-    }
-}
-
 lsp.format_on_save({
     format_opts = {
         async = false,
@@ -30,25 +19,19 @@ lsp.format_on_save({
     },
     servers = {
         ['lua_ls'] = { 'lua' },
-        -- ['gopls'] = { 'go' },
-        --['rust_analyzer'] = {'rust'},
-        -- if you have a working setup with null-ls
-        -- you can specify filetypes it can format.
-        -- ['null-ls'] = {'javascript', 'typescript'},
     }
 })
 
 lsp.setup()
 
-local lspconfig = require('lspconfig')
-
--- (Optional) Configure lua language server for neovim
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-
 local cmp = require('cmp')
 
 cmp.setup({
-    mapping = {
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    }
+    mapping = cmp.mapping.preset.insert {
+        ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+    },
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
 })
