93ad11c11cbb0a5ad3f65a199bf9de7549f23155
lailokkei <lailokkay@gmail.com>
2024-05-01 13:25:04 +0100

    shell stuff

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,15 +1,18 @@
-function prompt {
-  $loc = $executionContext.SessionState.Path.CurrentLocation;
-
-  $out = ""
-  if ($loc.Provider.Name -eq "FileSystem") {
-    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-  }
-  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-  return $out
-}
-
 Set-Alias ls exa
 
-$POSH_CONFIG = Join-Path(Split-Path $PSScriptRoot) theme.omp.json
-oh-my-posh init pwsh --config $POSH_CONFIG | Invoke-Expression
+$CONFIG_PATH = Join-Path(Split-Path $PSScriptRoot) starship.toml
+$ENV:STARSHIP_CONFIG = $CONFIG_PATH
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+function Invoke-Starship-PreCommand {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+  $prompt = "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem")
+  {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $host.ui.Write($prompt)
+}
+
+Invoke-Expression (&starship init powershell)
